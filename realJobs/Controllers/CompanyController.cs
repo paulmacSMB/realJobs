76308f3cedb8c080e1540efd3555709ad6003d97
Paul McNamara <paulmac445@gmail.com>
2025-05-07 13:47:24 -0400

    Adding company endpoint to gather url data

---
+++ realJobs/Controllers/CompanyController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using realJobs.DTOs;
+
+namespace realJobs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post([FromBody] List<CompanyDto> companyUrlData)
+        {
+            if (companyUrlData == null || companyUrlData.Count == 0)
+                return BadRequest("No data received.");
+
+            return Ok(new {message = "Data Received", count = companyUrlData.Count });
+        }
+    }
+}
---
+++ realJobs/DTOs/CompanyDto.cs
@@ -0,0 +1,19 @@
+﻿using Data.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace realJobs.DTOs
+{
+    public class CompanyDto
+    {
+        public int CompanyId { get; set; }
+        public string CompanyName { get; set; }
+        public string Domain { get; set; }
+        public string Subdomain { get; set; }
+        public string JobSearchPath { get; set; }
+        public string JobSearchUrl { get; set; }
+        public DateTime LastVerified { get; set; }
+        public string Notes { get; set; }
+
+        public ICollection<SearchStrategy> SearchStrategies { get; set; } = new List<SearchStrategy>();
+    }
+}
