fb8dd7166b8fc9df61e99a5721ae1af84f2b5ad0
Paul McNamara <paulmac445@gmail.com>
2025-05-10 19:48:25 -0400

    Fixed mapping objects

--- Logic/DTOs/SearchStrategyDto.cs
+++ Logic/DTOs/SearchStrategyDto.cs
@@ -2,11 +2,14 @@
 {
     public class SearchStrategyDto
     {
+        public int StrategyId { get; set; }
+        public int CompanyId { get; set; }
         public string CompanyName { get; set; }
         public string SearchType { get; set; }
         public string ExampleQuery { get; set; }
         public string Method { get; set; }
         public string MatchPattern { get; set; }
         public Dictionary<string, string> Headers { get; set; }
+        public CompanyDto Company { get; set; }
     }
 }
